5d61425a4fabe3522bd80c95d9f82f1c553bae34
Bilyee <bmaringonzalez.cf@iesesteveterradas.cat>
2026-03-10 17:54:26 +0100

    jjj

--- lliga/futbol/admin.py
+++ lliga/futbol/admin.py
@@ -7,10 +7,29 @@ admin.site.register(Equip)
 admin.site.register(Jugador)
 
 class EventInLine(admin.StackedInline):
+    exclude = ["detalls"]
     model = Event
     extra = 2
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # filtrem els jugadors i només deixem els que siguin d'algun dels 2 equips (local o visitant)
+        if db_field.name == "jugador":
+            partit_id = request.resolver_match.kwargs['object_id']
+            partit = Partit.objects.get(id=partit_id)
+            jugadors_local = [jugador.id for jugador in partit.equip_local.jugadors.all()]
+            jugadors_visitant = [jugador.id for jugador in partit.equip_visitant.jugadors.all()]
+            jugadors = jugadors_local + jugadors_visitant
+            kwargs["queryset"] = Jugador.objects.filter(id__in=jugadors)
+        # filtrem els equips i només deixem els 2 equips del partit
+        elif db_field.name == "equip":
+            partit_id = request.resolver_match.kwargs['object_id']
+            partit = Partit.objects.get(id=partit_id)
+            equips = [partit.equip_local.id, partit.equip_visitant.id]
+            kwargs["queryset"] = Equip.objects.filter(id__in=equips)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)            
 
 class PartitAdmin(admin.ModelAdmin):
+    search_fields = ["lliga__nom"]
+    list_display = ["lliga", "equip_local", "equip_visitant"]
     inlines = [EventInLine,]
 
 admin.site.register(Partit, PartitAdmin)
